0a9890ee38a4a8df5ae1d7ee258fda6087026532
Milos Srejic <srejicmilos@hotmail.com>
2021-09-02 18:28:57 +0200

    Added error page context

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Error.css';
+import { useHistory } from 'react-router-dom';
 
 const Error = () => {
+  const history = useHistory();
   return (
-    <div>
-      <h1>ERROR 404</h1>
+    <div className="error">
+      <h1>ERROR 404 PAGE NOT FOUND</h1>
+      <button
+        className="error__button"
+        onClick={() => {
+          history.push('/');
+        }}
+      >
+        HOME PAGE
+      </button>
     </div>
   );
 };
