030334b72c119e75e8e589600202953ee440ff65
Aleksandar Lazic <aleksandar.lazic.p@gmail.com>
2021-09-05 13:21:28 +0200

    Install necessary dependencies and updates

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,20 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { userReducer } from './ducks/User';
-import createSagaMiddleware from 'redux-saga';
-import { watcherSaga } from './sagas/rootSaga';
-import { postReducer } from './ducks/Post';
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { userReducer } from "./ducks/User";
+import createSagaMiddleware from "redux-saga";
+import { watcherSaga } from "./sagas/rootSaga";
+import { postReducer } from "./ducks/Post";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({
-  user: userReducer,
-  post: postReducer,
+	user: userReducer,
+	post: postReducer,
 });
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
-const store = createStore(reducer, applyMiddleware(...middleware));
+const store = createStore(
+	reducer,
+	composeWithDevTools(applyMiddleware(...middleware))
+);
 
 sagaMiddleware.run(watcherSaga);
 
